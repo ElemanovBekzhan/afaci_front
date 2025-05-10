b687ee1f0cad3fa7ee74a4c85a7656d79748ed34
ElemanovBekzhan <94730786+ElemanovBekzhan@users.noreply.github.com>
2025-05-10 17:30:53 +0600

    Fix National Page for new photo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,21 @@ import './Header.css'; // стили для шапки
 function Header() {
     return (
         <header className="header">
-            <nav className="nav">
-                <div className="logo">AFACI</div>
-                <ul className="nav-links">
-                    <li><a href="/">Главная</a></li>
-                    <li><a href="/products">Продукты по регионам</a></li>
-                    <li><a href="/national">Национальные блюда</a></li>
-                    <li><a href="#about">О нас</a></li>
-                    {/*<li><a href="/productDetail">Product Detail Test</a></li>*/}
-                </ul>
-                <div className="auth">
-                    <a href="/auth" className="login">Войти</a>
-                </div>
-            </nav>
+            <div className={"container header-container"}>
+                <nav className="nav">
+                    <div className="logo">AFACI</div>
+                    <ul className="nav-links">
+                        <li><a href="/">Главная</a></li>
+                        <li><a href="/products">Продукты по регионам</a></li>
+                        <li><a href="/national">Национальные блюда</a></li>
+                        <li><a href="#about">О нас</a></li>
+                        {/*<li><a href="/productDetail">Product Detail Test</a></li>*/}
+                    </ul>
+                    <div className="auth">
+                        <a href="/auth" className="login">Войти</a>
+                    </div>
+                </nav>
+            </div>
         </header>
     );
 }
